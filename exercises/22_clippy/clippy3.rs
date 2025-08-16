a5e333edda0956952245bd56191a8a1f46c0257b
CyberStackPro <yeabsragebriel@gmail.com>
2025-08-16 11:34:05 +0300

    apply clippy solutions

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -5,8 +5,11 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        println!("{:?}", my_option.unwrap());
+    // if my_option.is_none() {
+    //     println!("{:?}", my_option.unwrap());
+    // }
+    if let Some(value) = my_option{
+        println!("{:?}", value);
     }
 
     let my_arr = &[
@@ -21,7 +24,10 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+
+    // value_a = value_b;
+    // value_b = value_a;
+
+    std::mem::swap(&mut value_a,&mut  value_b);
     println!("value a: {value_a}; value b: {value_b}");
 }
